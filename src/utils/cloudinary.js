4df36b8de400b967a002b8696284d6fd9b74c72c
vishnukant <vishnukantray10@>
2026-01-03 20:20:17 +0530

    file upload and multer middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary"
+import { publicDecrypt } from "crypto"
+import { on } from "events"
+import { response } from "express"
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log("file is uploded on cloudinary",
+            response.url
+        );
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+    }
+}
+
+
+export { uploadOnCloudinary }
